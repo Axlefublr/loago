fff1c7f3badd11f018380ff6e5c7718a4c10b6b7
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-27 00:54:51 +0800

    add skeleton

---
+++ src/args.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+pub struct Args {
+	#[command(subcommand)]
+	action: Action
+}
+
+#[derive(Subcommand)]
+pub enum Action {
+	#[command(visible_alias = "d")]
+	Do,
+	#[command(visible_alias = "v")]
+	#[command(visible_alias = "list")]
+	#[command(visible_alias = "look")]
+	View,
+	#[command(visible_alias = "r")]
+	#[command(visible_alias = "delete")]
+	Remove
+}
