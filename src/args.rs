e00960c45f863b5ee73c8c5b458cc778ae082159
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-27 01:04:10 +0800

    add JsonRepo struct

--- src/args.rs
+++ src/args.rs
@@ -4,19 +4,19 @@ use clap::Subcommand;
 #[derive(Parser)]
 #[command(author, version, about)]
 pub struct Args {
-	#[command(subcommand)]
-	action: Action
+    #[command(subcommand)]
+    action: Action,
 }
 
 #[derive(Subcommand)]
 pub enum Action {
-	#[command(visible_alias = "d")]
-	Do,
-	#[command(visible_alias = "v")]
-	#[command(visible_alias = "list")]
-	#[command(visible_alias = "look")]
-	View,
-	#[command(visible_alias = "r")]
-	#[command(visible_alias = "delete")]
-	Remove
+    #[command(visible_alias = "d")]
+    Do,
+    #[command(visible_alias = "v")]
+    #[command(visible_alias = "list")]
+    #[command(visible_alias = "look")]
+    View,
+    #[command(visible_alias = "r")]
+    #[command(visible_alias = "delete")]
+    Remove,
 }
