9bc222ad5e8e2ce360b77886124d56722ad00423
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-28 01:18:30 +0800

    remove errors and error handling for now binary function

--- src/errors.rs
+++ src/errors.rs
@@ -1,23 +1,2 @@
 use std::error::Error;
 use std::fmt;
-
-pub trait AsErrStr {
-    fn as_str(&self) -> &'static str;
-}
-
-#[derive(Debug)]
-pub struct DataDirNotFoundError;
-
-impl AsErrStr for DataDirNotFoundError {
-    fn as_str(&self) -> &'static str {
-        "local data directory wasn't found"
-    }
-}
-
-impl Error for DataDirNotFoundError {}
-
-impl fmt::Display for DataDirNotFoundError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.as_str())
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::path::PathBuf;
 use loago::APP_NAME;
-use loago::errors::AsErrStr;
 
 mod args;
 
@@ -8,16 +7,6 @@ pub const DATA_FILE_NAME: &str = "loago.json";
 
 fn main() {
     println!("Hello, world!");
-}
-
-pub trait UserFacing<T> {
-    fn to_friendly(self) -> Result<T, &'static str>;
-}
-
-impl<T, E: AsErrStr> UserFacing<T> for Result<T, E> {
-    fn to_friendly(self) -> Result<T, &'static str> {
-        self.map_err(|err| err.as_str())
-    }
 }
 
 pub fn default_path() -> Result<PathBuf, &'static str> {
