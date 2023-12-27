d25dfdf4c2d934f7a083d2fbf17a72bb6dd0576a
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-28 00:50:44 +0800

    use AsErrStr trait to show user facing error message without allocation

--- src/errors.rs
+++ src/errors.rs
@@ -1,18 +1,23 @@
 use std::error::Error;
 use std::fmt;
 
+pub trait AsErrStr {
+    fn as_str(&self) -> &'static str;
+}
+
+#[derive(Debug)]
 pub struct DataDirNotFoundError;
+
+impl AsErrStr for DataDirNotFoundError {
+    fn as_str(&self) -> &'static str {
+        "local data directory wasn't found"
+    }
+}
 
 impl Error for DataDirNotFoundError {}
 
 impl fmt::Display for DataDirNotFoundError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "local data directory wasn't found")
-    }
-}
-
-impl fmt::Debug for DataDirNotFoundError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(self, f)
+        write!(f, "{}", self.as_str())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use loago::errors::AsErrStr;
+
 mod args;
 
 fn main() {
     println!("Hello, world!");
+}
+
+pub trait UserFacing<T> {
+    fn to_friendly(self) -> Result<T, &'static str>;
+}
+
+impl<T, E: AsErrStr> UserFacing<T> for Result<T, E> {
+    fn to_friendly(self) -> Result<T, &'static str> {
+        self.map_err(|err| err.as_str())
+    }
 }
