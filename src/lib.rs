db86b44ca3ff036b202fd4b8b2aa5331c8283792
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-30 00:54:56 +0800

    implement Display for OutputTasks

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 use chrono::NaiveDateTime;
 use chrono::Utc;
@@ -21,8 +22,9 @@ impl Tasks {
         }
     }
 
-    pub fn output(self) -> OutputTasks {
-        let now = now();
+    pub fn output(self) -> OutputTasks { self.output_when(now()) }
+
+    fn output_when(self, now: NaiveDateTime) -> OutputTasks {
         let mut output: Vec<KeyToDiff> = self
             .0
             .into_iter()
@@ -38,6 +40,30 @@ fn now() -> NaiveDateTime { Utc::now().naive_utc() }
 type KeyToDiff = (String, i64);
 
 pub struct OutputTasks(Vec<KeyToDiff>);
+
+impl fmt::Display for OutputTasks {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut length = 0;
+        self.0.iter().for_each(|(task_name, _)| {
+            let task_name_len = task_name.len();
+            if task_name_len > length {
+                length = task_name_len;
+            }
+        });
+        let mut buffer = String::new();
+        for (key, days_diff) in self.0.iter() {
+            let whitespace = " ".repeat(length - key.len());
+            buffer.push_str(key);
+            buffer.push_str(&whitespace);
+            buffer.push(' ');
+            buffer.push('—');
+            buffer.push(' ');
+            buffer.push_str(&days_diff.to_string());
+            buffer.push('\n')
+        }
+        write!(f, "{}", buffer)
+    }
+}
 
 #[cfg(test)]
 mod tasks {
@@ -58,6 +84,14 @@ mod tasks {
             map.insert(String::from("exercise"), december);
             Self(map)
         }
+
+        fn different_days() -> Self {
+            let mut map = HashMap::new();
+            map.insert(String::from("dust"), november(1));
+            map.insert(String::from("vacuum"), november(2));
+            map.insert(String::from("exercise"), november(3));
+            Self(map)
+        }
     }
 
     fn december() -> NaiveDateTime {
@@ -67,8 +101,8 @@ mod tasks {
             .unwrap()
     }
 
-    fn november() -> NaiveDateTime {
-        NaiveDate::from_ymd_opt(2023, 11, 20)
+    fn november(day: u32) -> NaiveDateTime {
+        NaiveDate::from_ymd_opt(2023, day, 20)
             .unwrap()
             .and_hms_opt(0, 0, 0)
             .unwrap()
@@ -101,5 +135,13 @@ mod tasks {
         for (_, actual_diff) in test_tasks.0 {
             assert_eq!(actual_diff, expected_diff.num_days());
         }
+    }
+
+    #[test]
+    fn output_display() {
+        let test_tasks = Tasks::different_days().output_when(december());
+        let expected = String::from("exercise — 275\nvacuum   — 303\ndust     — 334\n");
+        let actual = test_tasks.to_string();
+        assert_eq!(expected, actual);
     }
 }
