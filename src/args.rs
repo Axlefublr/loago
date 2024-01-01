a1bc7118a5c5b1eac132bb82f922c82a5311aa80
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2024-01-02 04:19:04 +0800

    impl binary functionality

--- src/args.rs
+++ src/args.rs
@@ -1,21 +1,53 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+
 use clap::Parser;
 use clap::Subcommand;
+use loago::Tasks;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 pub struct Args {
     #[command(subcommand)]
-    action: Action,
+    pub action: Action,
 }
 
 #[derive(Subcommand)]
 pub enum Action {
     #[command(visible_alias = "add")]
     #[command(visible_alias = "new")]
-    Do,
+    Do { tasks: Vec<String> },
     #[command(visible_alias = "list")]
     #[command(visible_alias = "look")]
-    View,
+    View { tasks: Option<Vec<String>> },
     #[command(visible_alias = "delete")]
-    Remove,
+    Remove { tasks: Vec<String> },
+}
+
+impl Action {
+    pub fn execute(self, data_file: File, mut tasks: Tasks) {
+        match self {
+            Self::Do { tasks: provided } => {
+                tasks.update_multiple(provided);
+                save(tasks, data_file);
+            },
+            Self::Remove { tasks: provided } => {
+                tasks.remove_multiple(&provided);
+                save(tasks, data_file);
+            },
+            Self::View { tasks: provided } => {
+                if let Some(provided) = provided {
+                    tasks.keep_multiple(provided);
+                }
+                println!("{}", tasks.output_days());
+            },
+        }
+    }
+}
+
+fn save(tasks: Tasks, mut data_file: File) {
+    let map: HashMap<String, String> = tasks.into();
+    let json = serde_json::to_string_pretty(&map).unwrap();
+    data_file.write_all(json.as_bytes()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@ use std::fs;
 use std::fs::OpenOptions;
 use std::io;
 use std::io::Read;
+use std::io::Seek;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 
-use chrono::NaiveDateTime;
+use args::Args;
+use clap::Parser;
 use loago::Tasks;
 use loago::APP_NAME;
 
@@ -18,13 +20,16 @@ const DATA_FILE_NAME: &str = "loago.json";
 const EMPTY_JSON_FILE_CONTENT: &[u8; 2] = b"{}";
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let Args { action } = Args::parse();
     let data_dir = app_data_dir()?;
     let path = ensure_exists(data_dir, DATA_FILE_NAME)?;
     let mut file = OpenOptions::new().read(true).write(true).open(path)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    file.read_to_string(&mut contents)?;
+    file.rewind()?;
     let data: HashMap<String, String> = serde_json::from_str(&contents)?;
     let tasks = Tasks::try_from(data)?;
+    action.execute(file, tasks);
     Ok(())
 }
 
@@ -37,7 +42,7 @@ fn app_data_dir() -> Result<PathBuf, &'static str> {
 fn ensure_exists(data_dir: PathBuf, data_file: impl AsRef<Path>) -> Result<PathBuf, io::Error> {
     fs::create_dir_all(&data_dir)?;
     let full_path = data_dir.join(data_file);
-    match OpenOptions::new().write(true).create_new(true).open(full_path) {
+    match OpenOptions::new().write(true).create_new(true).open(&full_path) {
         Ok(mut file) => {
             file.write_all(EMPTY_JSON_FILE_CONTENT)?;
             file.flush()?;
